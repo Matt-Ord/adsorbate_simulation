b0585f0027ce796f2d42784c69f937c8e1f9c617
Matthew Ord <matthew.ord1@gmail.com>
2024-11-18 15:08:08 +0000

    Initial Commit

--- slate/array/__init__.py
+++
@@ -1,7 +0,0 @@
-"""The array type for Slate."""
-
-from __future__ import annotations
-
-from .array import SlateArray
-
-__all__ = ["SlateArray"]
--- slate/explicit_basis/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Basis with explicit basis vectors."""
-
-from __future__ import annotations
-
-from ._explicit_basis import ExplicitBasis, ExplicitUnitaryBasis
-
-__all__ = ["ExplicitBasis", "ExplicitUnitaryBasis"]
